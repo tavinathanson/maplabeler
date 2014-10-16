3b208c8b371ee7258d7488c2fb2e7a9c14d5dc0e
Tavi Nathanson <tavi.nathanson@gmail.com>
2014-10-16 14:58:56 -0400

    Adding a swig template, and react

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express')
+var swig  = require('swig');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'swig')
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+    var template = swig.compileFile('index.html');
+    var output = template({
+        title: 'Title',
+        message: 'Message'
+    });
+    response.send(output)
 })
 
 app.listen(app.get('port'), function() {
